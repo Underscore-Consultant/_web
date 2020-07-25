1de82151a3c235f30fab920c4341026df37a6c3e
olatunde omotolani <codester291@gmail.com>
2020-07-25 15:03:03 +0100

    :zap: :zap: added links to social media accounts :zap: :zap:

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,49 @@
 <template>
   <div>
     <div class="parent">
-      <div style="">
-          <br><br><br><br>
+      <div style="background: #333;">
+        <br />
+        <br />
+        <br />
+        <br />
+        <div>
           <span>underscore.consultant@gmail.com</span>
-          <br>
+          <br />
           <span>08142558011 | 08085816733</span>
-          <br>
+          <br />
           <span>561, Sabo-Yaba, Alagomeji, Lagos</span>
-          <br><br><br><br><br>
-          <img src="../assets/gifs/lines.gif" alt="">
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div class="social">
+          <span>
+            <a href>
+              <img src="../assets/svg/facebooks.svg" alt height="50" width="50" />
+            </a>
+            <a href>
+              <img src="../assets/svg/instagram.svg" alt height="50" width="50" />
+            </a>
+            <a href>
+              <img src="../assets/svg/twitter.svg" alt height="50" width="50" />
+            </a>
+            <a href>
+              <img src="../assets/svg/github.svg" alt height="50" width="50" />
+            </a>
+          </span>
+        </div>
       </div>
       <div style="background: #0066f5">
         <h1>Contact Us</h1>
@@ -36,7 +70,8 @@ export default {
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap");
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap");
 .parent {
   display: grid;
   grid-template-columns: minmax(150px, 25%) 1fr;
@@ -57,8 +92,8 @@ label {
 }
 
 span {
-    font-family: 'Open Sans', sans-serif;
-    color: #0066f5;
+  font-family: "Source Sans Pro", sans-serif;
+  color: floralwhite;
 }
 
 label {
@@ -70,11 +105,24 @@ textarea {
   border-style: none;
   padding: 15px;
   outline: none;
-  /* box-shadow: 12px 12px 16px 0 #ffffff,
-         -8px -8px 12px 0 #00ffff; */
-  /* background: transparent; */
   width: 50%;
   transition: 0.9s;
+  color: #666;
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+input:focus,
+textarea:focus {
+  border-style: none;
+  padding: 15px;
+  outline: none;
+  border-radius: 30px;
+  background: #55b9f3;
+  box-shadow: inset 20px 20px 60px #489dcf, inset -20px -20px 60px #62d5ff;
+  width: 50%;
+  transition: 0.9s;
+  color: floralwhite;
+  font-family: "Source Sans Pro", sans-serif;
 }
 button {
   border-style: none;
@@ -84,10 +132,16 @@ button {
   width: 10%;
   font-weight: 900;
   transition: 0.9s;
+  outline: none;
 }
 button:hover {
   background: #add8e6;
   color: floralwhite;
+  border-radius: 50px;
   transition: 0.9s;
+}
+
+img {
+  padding: 10px;
 }
 </style>
